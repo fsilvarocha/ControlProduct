d31cefad29839049d4b5970d9db1a741cc6d39d9
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2022-07-27 13:42:06 -0300

    8Scaffolding_Controllers_Views

--- src/ControlProduct.App/Data/ApplicationDbContext.cs
+++ src/ControlProduct.App/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ControlProduct.App.ViewModels;
 
 namespace ControlProduct.App.Data
 {
@@ -12,5 +13,6 @@ namespace ControlProduct.App.Data
             : base(options)
         {
         }
+        public DbSet<ControlProduct.App.ViewModels.ProdutoViewModel> ProdutoViewModel { get; set; }
     }
 }
